30466006d6aef093d6e58723b12f974fbd095bda
hzx-123-wq <hanzixian1234@163.com>
2024-12-01 22:20:48 +0800

    Update run-vis.py

--- FNI-DETR/run-vis.py
+++ FNI-DETR/run-vis.py
@@ -5,4 +5,11 @@ model = RTDETR("ultralytics/cfg/models/rt-detr/detr-all-l.yaml")  # build a new 
 if __name__ == '__main__':
 
     # Use the model
-    model.train(data="ultralytics/cfg/datasets/VisDrone.yaml", cfg="ultralytics/cfg/default.yaml", epochs=100)  # train the model
+    model.train(data="ultralytics/cfg/datasets/VisDrone.yaml", 
+                imgsz=640, 
+                epochs=100, 
+                batch=4, 
+                workers=4, 
+                project='runs/train-VisDrone'
+                name='FNI-DETR'
+               )  # train the model
