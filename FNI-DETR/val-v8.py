b864ffe7bdf54292e1a7729f27e71cc4bb614f15
hzx-123-wq <hanzixian1234@163.com>
2024-12-01 22:29:21 +0800

    Delete FNI-DETR/val-v8.py

--- FNI-DETR/val-v8.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import RTDETR
-
-model = RTDETR("runs/detect/test/best.pt")
-
-
-if __name__ == '__main__':
-
-    # Use the model
-    model.val(data="ultralytics/cfg/datasets/coco.yaml", cfg="ultralytics/cfg/default.yaml", epochs=1)  # train the model
