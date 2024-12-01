74058aaf0ccb64d4f0dcb258b23e9661c7ba53ef
hzx-123-wq <hanzixian1234@163.com>
2024-12-01 22:18:28 +0800

    Update run-coco.py

--- FNI-DETR/run-coco.py
+++ FNI-DETR/run-coco.py
@@ -5,4 +5,11 @@ model = RTDETR("ultralytics/cfg/models/rt-detr/detr-all-l.yaml")  # build a new 
 if __name__ == '__main__':
 
     # Use the model
-    model.train(data="ultralytics/cfg/datasets/VisDrone.yaml", cfg="ultralytics/cfg/default.yaml", epochs=100)  # train the model
+    model.train(data="ultralytics/cfg/datasets/coco.yaml", 
+                imgsz=640, 
+                epochs=100, 
+                batch=4, 
+                workers=4, 
+                project='runs/train-coco'
+                name='FNI-DETR'
+               )  # train the model
